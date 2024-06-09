774758d952625a5ad97f4b784f336eb84fb9bcf5
Ashley Roby <ashleyrobycreates@gmail.com>
2024-06-08 23:25:01 -0400

    JS code added

---
+++ script.js
@@ -0,0 +1,7 @@
+let button = document.querySelector("button");
+
+        button.addEventListener("click", function() {
+    
+            alert("I paint, draw, watch movies, and love any outdoor activity!");
+       
+        });
